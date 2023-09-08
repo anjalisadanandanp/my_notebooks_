f8deb7c0063d73755b3872308b8f507994bc4a22
anjalisadanandanp <anjalip1@iisc.ac.in>
2023-09-08 18:06:47 +0530

    initial commit

---
+++ inception_resnet_densenet/tutorial_01/readme.txt
@@ -0,0 +1,1 @@
+source: https://uvadlc-notebooks.readthedocs.io/en/latest/tutorial_notebooks/tutorial5/Inception_ResNet_DenseNet.html
---
+++ normal_rv/my_code.py
@@ -0,0 +1,34 @@
+#create a plot of a normal distribution
+
+#import the necessary libraries
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+
+#function to plot a normal distribution
+#parameters: mean, standard deviation
+def plot_normal(mean, std):
+
+    #create a line plot of the normal distribution
+    x = np.linspace(mean - 4*std, mean + 4*std, 100)
+    plt.plot(x, stats.norm.pdf(x, mean, std), color='r')
+
+    #set the title
+    plt.title('Normal Distribution')
+
+    #set the x-axis label
+    plt.xlabel('X')
+
+    #set the y-axis label
+    plt.ylabel('Probability Density')
+
+    #gridlines
+    plt.grid(True)
+
+    #show the plot
+    plt.savefig('normal_'+ str(mean) + "_" + str(std) +'_.png')
+
+#call the function
+plot_normal(60, 20)
+
+
